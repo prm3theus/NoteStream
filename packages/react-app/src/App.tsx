def5cbb316e4b5d464f1c2d8e6bc54beaa654f87
Tom French <tom@tomfren.ch>
2020-04-20 15:37:12 +0100

    build: strip remainder of code

--- packages/react-app/src/App.tsx
+++ packages/react-app/src/App.tsx
@@ -1,54 +1,7 @@
-import React, { ReactElement, useState, useEffect } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
-
-const useStyles = makeStyles((theme) => ({
-  layout: {
-    width: 'auto',
-    marginLeft: theme.spacing(2),
-    marginRight: theme.spacing(2),
-    [theme.breakpoints.up(1200 + theme.spacing(2) * 2)]: {
-      width: 1200,
-      marginLeft: 'auto',
-      marginRight: 'auto',
-    },
-  },
-  paper: {
-    padding: theme.spacing(2),
-    [theme.breakpoints.up(800 + theme.spacing(3) * 2)]: {
-      padding: theme.spacing(3),
-    },
-  },
-  pageElement: {
-    marginTop: theme.spacing(1.5),
-    marginBottom: theme.spacing(1.5),
-    [theme.breakpoints.up(800 + theme.spacing(3) * 2)]: {
-      marginTop: theme.spacing(3),
-      marginBottom: theme.spacing(3),
-    },
-  },
-  icon: {
-    marginRight: theme.spacing(2),
-  },
-  // button: {
-  //   marginRight: theme.spacing(2),
-  // },
-  title: {
-    flexGrow: 1,
-  },
-}));
-
-const NETWORK_ID: number = parseInt(process.env.REACT_APP_NETWORK_ID as string, 10);
+import React, { ReactElement } from 'react';
 
 const App = (): ReactElement => {
-  const classes = useStyles();
-
-  const [streamContractInstance, setStreamContractInstance] = useState({});
-  const [value, setValue] = useState(0);
-  const [aztec, setAztec] = useState({} as any);
-
-  return <main className={classes.layout}></main>;
+  return <main></main>;
 };
 
 export default App;
--- packages/react-app/src/index.tsx
+++ packages/react-app/src/index.tsx
@@ -1,23 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { ApolloClient, ApolloProvider, HttpLink, InMemoryCache } from '@apollo/client';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-const client = new ApolloClient({
-  cache: new InMemoryCache(),
-  link: new HttpLink({
-    uri: process.env.REACT_APP_SUBGRAPH_URL,
-  }),
-});
-
-ReactDOM.render(
-  <ApolloProvider client={client}>
-    <App />
-  </ApolloProvider>,
-  document.getElementById('root'),
-);
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
